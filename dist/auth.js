d705523094caca04dfebb904228bdda70fd3ec07
Jonas Brahmst <jonasbrahmst@gmail.com>
2024-08-17 16:33:20 +0200

    Commit

--- dist/auth.js
+++ dist/auth.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Mira = void 0;
-// src/auth.ts
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const secret = process.env.MIRA_SECRET;
